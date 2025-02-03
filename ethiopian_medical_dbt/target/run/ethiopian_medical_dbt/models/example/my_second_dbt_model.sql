d5ac5e6cdad4815a19de389f91eba4f1c4700c87
Kaleab <kaleabb266@gmail.com>
2025-02-03 15:36:34 +0300

    loggof object detection

---
+++ ethiopian_medical_dbt/target/run/ethiopian_medical_dbt/models/example/my_second_dbt_model.sql
@@ -0,0 +1,11 @@
+
+  create view "telegram_data"."public"."my_second_dbt_model__dbt_tmp"
+    
+    
+  as (
+    -- Use the `ref` function to select from other models
+
+select *
+from "telegram_data"."public"."my_first_dbt_model"
+where id = 1
+  );
---
+++ ethiopian_medical_dbt/target/run/ethiopian_medical_dbt/models/models/transformations/cleaned_messages.sql
@@ -0,0 +1,20 @@
+
+  create view "telegram_data"."public"."cleaned_messages__dbt_tmp"
+    
+    
+  as (
+    SELECT
+    message_id,
+    channel,
+    sender_id,
+    text,
+    date::date AS message_date,
+    CASE
+        WHEN text IS NULL THEN 'No text'
+        ELSE text
+    END AS cleaned_text
+FROM
+    telegram_data
+WHERE
+    text IS NOT NULL
+  );
